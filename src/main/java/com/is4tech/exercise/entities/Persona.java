2a2c73e9bc3fb54d7fa75a5635e641d373634239
wcabrera-2022060 <wcabrera-2022060@kinal.edu.gt>
2024-09-16 18:07:33 -0600

    entidad Persona finalizada

---
+++ src/main/java/com/is4tech/exercise/entities/Persona.java
@@ -0,0 +1,24 @@
+package com.is4tech.exercise.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "persona")
+public class Persona {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_persona")
+    private Long idPersona;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "id_tipo_documento")
+    private Long idTipoDocumento;
+
+    @Column(name = "id_direccion")
+    private Long idDireccion;
+}
